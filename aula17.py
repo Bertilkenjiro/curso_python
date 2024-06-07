da1ff48e6c8a3ad9dd404639c3ffcc99b5107a42
Bertil kenjiro <bertiljunior@gmail.com>
2024-06-06 22:20:46 -0300

    Aula 17

---
+++ aula17.py
@@ -0,0 +1,32 @@
+# if / elif       / else
+# se / se não se / se não
+condicao = True
+
+# if condicao:
+#    print('Este é o código do if')
+#else:
+#    print('Este é o else do primeiro if')
+    
+#print('Fora do if')
+
+condicao1 = False
+condicao2 = True
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+if condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')
+    
+if 10 == 10:
+    print('Outro if')
+    
+print('Fora do if')
