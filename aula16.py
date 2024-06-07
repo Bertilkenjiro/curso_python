ee1638e0b98158ea294346dae266b762c53acce2
Bertil kenjiro <bertiljunior@gmail.com>
2024-06-06 21:44:42 -0300

    complemento da aula 16

--- aula16.py
+++ aula16.py
@@ -4,7 +4,11 @@ entrada = input('Você quer "entrar" ou "sair"? ')
 
 if entrada == 'entrar':
     print('Você entrou no sistema')
+    
+    print(12341234)
 elif entrada == 'sair':
     print('Você saiu do sistema')
 else:
     print('Você nao digitou nem entrar nem sair.')
+    
+print('FORA DOS BLOCOS')
