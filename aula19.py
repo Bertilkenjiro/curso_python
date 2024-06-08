7896a3e82c35ecc8f422bcbdc09869b14a1c69b8
Bertil kenjiro <bertiljunior@gmail.com>
2024-06-07 21:08:46 -0300

    alterações

--- aula19.py
+++ aula19.py
@@ -8,3 +8,10 @@ OP       Significado         Exempo (Tue)
 ==       igual               'a' == 'a' 
 !=       diferente           'a' != 'b'
 """
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print(diferente)
